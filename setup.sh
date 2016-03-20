950446c60dd52e932cd3f6673bf0452c0745ae12
Andernerd <andernerd@gmail.com>
2016-03-20 14:16:48 -0600

    added some functionality to setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
-apt-get update
-apt-get upgrade
-apt-get install vim python-pip
-pacman -Syu vim
+mkdir ~/.i3
+pacman -Syu vim neovim i3-wm i3status i3lock
+cp .vimrc ~/.vimrc
+cp .i3/config ~/.i3
 tar xvfz clewn.tar.gz
