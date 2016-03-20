917fea6e1ed350bbb6a7b8e88a2fd1032d5e1c14
Andernerd <andernerd@gmail.com>
2016-03-20 14:19:03 -0600

    added dmenu installation

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 mkdir ~/.i3
-sudo pacman -Syu vim neovim i3-wm i3status i3lock
+sudo pacman -Syu vim neovim i3-wm i3status i3lock dmenu
 cp .vimrc ~/.vimrc
 cp .i3/config ~/.i3
 tar xvfz clewn.tar.gz
