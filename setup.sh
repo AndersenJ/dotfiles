d0386c3fc413e90f7db0c2c2ab63de13034054b1
Andernerd <andernerd@gmail.com>
2016-03-21 11:32:01 -0600

    did some stuff

--- setup.sh
+++ setup.sh
@@ -1,5 +1,9 @@
 mkdir ~/.i3
-sudo pacman -Syu vim neovim i3-wm i3status i3lock dmenu
+sudo pacman -Syu vim neovim i3-wm i3status i3lock dmenu xorg-server xorg-utils xorg-xclipboard xorg-apps xterm xorg-xinit firefox networkmanager htop
 cp .vimrc ~/.vimrc
+cp .xinitrc ~/.xinitrc
+cp .vimperatorrc ~/.vimperatorrc
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+rm ~/.i3/config
 cp .i3/config ~/.i3
 tar xvfz clewn.tar.gz
