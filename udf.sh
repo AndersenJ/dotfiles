e3a47ba00d4abfc0e1de03a29de8fb8b5cc93a14
AndersenJ <johnathan.andersen@byu.edu>
2016-04-18 11:14:11 -0600

    added udf.sh

---
+++ udf.sh
@@ -0,0 +1,12 @@
+rm ~/.vimrc
+rm ~/.bashrc
+rm ~/.vimperatorrc
+rm ~/.gitconfig
+rm ~/.xinitrc
+rm ~/.Xresources
+cp ~/dotfiles/.vimrc ~/
+cp ~/dotfiles/.bashrc ~/
+cp ~/dotfiles/.vimperatorrc ~/
+cp ~/dotfiles/.gitconfig ~/
+cp ~/dotfiles/.xinitrc ~/
+cp ~/dotfiles/.Xresources ~/
