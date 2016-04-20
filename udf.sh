a62492ad9934cad7aba02910996ac2290c72b467
AndersenJ <johnathan.andersen@byu.edu>
2016-04-20 15:22:58 -0600

    added a .profile file for git bash and for OS X machines

--- udf.sh
+++ udf.sh
@@ -1,11 +1,13 @@
 rm ~/.vimrc
 rm ~/.bashrc
+rm ~/.profile
 rm ~/.vimperatorrc
 rm ~/.gitconfig
 rm ~/.xinitrc
 rm ~/.Xresources
 cp ~/dotfiles/.vimrc ~/
 cp ~/dotfiles/.bashrc ~/
+cp ~/dotfiles/.profile ~/
 cp ~/dotfiles/.vimperatorrc ~/
 cp ~/dotfiles/.gitconfig ~/
 cp ~/dotfiles/.xinitrc ~/
