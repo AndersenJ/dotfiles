ee3abc22d7aafa4b4947c33375d13991c7925f2d
AndersenJ <johnathan.andersen@byu.edu>
2016-04-20 15:27:26 -0600

    updated setup.sh to call udf.sh and to install git before trying to use git

--- setup.sh
+++ setup.sh
@@ -1,9 +1,8 @@
-mkdir ~/.i3
-sudo pacman -Syu vim neovim i3-wm i3status i3lock dmenu xorg-server xorg-utils xorg-xclipboard xorg-apps xterm xorg-xinit firefox networkmanager htop screenfetch
-cp .vimrc ~/.vimrc
-cp .xinitrc ~/.xinitrc
-cp .vimperatorrc ~/.vimperatorrc
+sudo pacman -Syu vim neovim i3-wm i3status i3lock dmenu xorg-server xorg-utils xorg-xclipboard xorg-apps xterm xorg-xinit firefox networkmanager htop screenfetch git
+chmod +x udf.sh
+./udf.sh
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+mkdir ~/.i3
 rm ~/.i3/config
 cp .i3/config ~/.i3
 tar xvfz clewn.tar.gz
