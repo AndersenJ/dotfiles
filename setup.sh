fc949aa0c0ed207b89493a3961f46eafcf47ee8f
AndersenJ <johnathan.andersen@byu.edu>
2016-05-02 12:15:52 -0600

    added support for fedora

--- setup.sh
+++ setup.sh
@@ -1,4 +1,8 @@
 sudo pacman -Syu vim neovim i3-wm i3status i3lock dmenu xorg-server xorg-utils xorg-xclipboard xorg-apps xterm xorg-xinit firefox networkmanager htop screenfetch git
+sudo dnf -y install dnf-plugins-core
+sudo dnf -y copr enable dperson/neovim
+sudo dnf -y install neovim
+sudo dnf install vim i3 firefox git htop python2-greenlet-devel
 chmod +x udf.sh
 ./udf.sh
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
