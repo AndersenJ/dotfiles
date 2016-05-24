020fd131103cddccc50d199ba6aab0e316edda2f
AndersenJ <johnathan.andersen@byu.edu>
2016-05-24 14:00:09 -0600

    added hack font installation for fedora

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
 sudo pacman -Syu vim neovim i3-wm i3status i3lock dmenu xorg-server xorg-utils xorg-xclipboard xorg-apps xterm xorg-xinit firefox networkmanager htop screenfetch git xclip emacs xfce4-terminal mutt
 sudo dnf -y install dnf-plugins-core
 sudo dnf -y copr enable dperson/neovim
-sudo dnf -y install neovim
-sudo dnf install vim i3 firefox git htop python2-greenlet-devel xclip emacs xfce4-terminal mutt
+sudo dnf -y copr enable heliocastro/hack-fonts
+sudo dnf install vim i3 firefox git htop python2-greenlet-devel xclip emacs xfce4-terminal mutt neovim hack-fonts
 sudo apt-get update && sudo apt-get upgrade
 sudo apt-get install vim i3-wm i3status i3lock i3 suckless-tools network-manager htop git xclip emacs xfce4-terminal mutt
 chmod +x udf.sh
