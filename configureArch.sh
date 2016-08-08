906fc5780cffbd2fc7028290417ec1df3fa465e6
AndersenJ <johnathan.andersen@byu.edu>
2016-08-08 11:20:33 -0600

    fixed (hopefully) errors in arch configuration script

--- configureArch.sh
+++ configureArch.sh
@@ -6,12 +6,13 @@ hwclock --systohc --utc
 locale-gen
 rm /etc/locale.gen
 cp ./locale.gen /etc/locale.gen
+rm /etc/locale.conf
 echo "LANG=en_US.UTF-8" > /etc/locale.conf
 export LANG=en_US.UTF-8
 echo "[multilib]" >> /etc/pacman.conf
 echo "Include = /etc/pacman.d/mirrorlist" >> /etc/pacman.conf
-useradd -m -g users -G wheel,storage,power -s /bin/bash johnathan
-pacman -Syu --noconfirm sudo alsa-utils
+pacman -Syu --noconfirm sudo alsa-utils zsh
+useradd -m -g users -G wheel,storage,power,audio -s /bin/zsh johnathan
 
 echo "use alsamixer to configure sound"
 echo "Don't forget to put Defaults insults in your sudoers file with the sudo visudo command!"
