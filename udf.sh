e1ca475f4c7bf887912a89dd6351126e8e946de0
AndersenJ <johnathan.andersen@byu.edu>
2016-08-30 13:47:05 -0600

    updated udf.sh to not update .zshrc

--- udf.sh
+++ udf.sh
@@ -8,7 +8,7 @@ rm ~/.xinitrc 2> /dev/null
 rm ~/.Xresources 2> /dev/null
 rm ~/.i3/config 2> /dev/null
 rm ~/.muttrc 2> /dev/null
-rm ~/.zshrc 2> /dev/null
+#rm ~/.zshrc 2> /dev/null
 mkdir ~/.i3 2> /dev/null
 mkdir ~/.config 2> /dev/null
 mkdir ~/.config/xfce4 2> /dev/null
@@ -24,6 +24,6 @@ cp ~/dotfiles/.xinitrc ~/
 cp ~/dotfiles/.Xresources ~/
 cp ~/dotfiles/.config/xfce4/terminal/terminalrc ~/.config/xfce4/terminal/terminalrc
 cp ~/dotfiles/.muttrc ~/
-cp ~/dotfiles/.zshrc ~/
+#cp ~/dotfiles/.zshrc ~/
 vim +PluginClean +PluginUpdate +qall
 source ~/.bashrc
