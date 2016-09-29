6dcca395bfbcc1a54d5fb32ee2c924af59c23a84
AndersenJ <johnathan.andersen@byu.edu>
2016-09-29 06:43:21 -0600

    removed output null-routing from pacman command in setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #Arch Linux
-sudo pacman -Syu cups dmenu emacs feh firefox fish git gvim htop i3-wm i3lock i3status lolcat lsof mutt networkmanager neovim net-tools pcmanfm powertop pv python2-neovim python-neovim rofi sbcl screenfetch scrot sway transmission-qt ttf-hack unzip wget xfce4-terminal xorg-apps xorg-server xorg-utils xorg-xclipboard xorg-xinit xclip xterm zsh zsh-syntax-highlighting 2> /dev/null
+sudo pacman -Syu cups dmenu emacs feh firefox fish git gvim htop i3-wm i3lock i3status lolcat lsof mutt networkmanager neovim net-tools pcmanfm powertop pv python2-neovim python-neovim rofi sbcl screenfetch scrot sway transmission-qt ttf-hack unzip wget xfce4-terminal xorg-apps xorg-server xorg-utils xorg-xclipboard xorg-xinit xclip xterm zsh zsh-syntax-highlighting
 
 #Fedora Linux
 sudo dnf -y install dnf-plugins-core 2> /dev/null
