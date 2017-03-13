232ae809d3f91f0a348264263eb4de5112d126ae
AndersenJ <johnathan.andersen@byu.edu>
2017-03-13 10:37:37 -0600

    formatted setup.sh for easier editing

--- setup.sh
+++ setup.sh
@@ -1,16 +1,90 @@
 #Arch Linux
-sudo pacman -Syu cups dmenu emacs feh firefox fish git gvim htop i3-wm i3lock i3status lolcat lsof mutt networkmanager neovim net-tools ntp pcmanfm powertop pv python2-neovim python-neovim rofi sbcl screenfetch scrot sl transmission-qt ttf-hack unzip wine wine_gecko wine-mono wget xfce4-terminal xorg-apps xorg-server xorg-utils xorg-xclipboard xorg-xinit xclip xterm zsh zsh-syntax-highlighting
+sudo pacman -Syu \
+    cups \
+    dmenu \
+    emacs \
+    feh \
+    firefox \
+    fish \
+    git \
+    gvim \
+    htop \
+    i3-wm \
+    i3lock \
+    i3status \
+    lolcat \
+    lsof \
+    mutt \
+    networkmanager \
+    neovim \
+    net-tools \
+    ntp \
+    pcmanfm \
+    powertop \
+    pv \
+    python2-neovim \
+    python-neovim \
+    rofi \
+    sbcl \
+    screenfetch \
+    scrot \
+    sl \
+    transmission-qt \
+    ttf-hack \
+    unzip \
+    wine \
+    wine_gecko \
+    wine-mono \
+    wget \
+    xfce4-terminal \
+    xorg-apps \
+    xorg-server \
+    xorg-utils \
+    xorg-xclipboard \
+    xorg-xinit \
+    xclip \
+    xterm \
+    zsh \
+    zsh-syntax-highlighting
+
 sudo systemctl enable ntpdate.service -q
 
 #Fedora Linux
 sudo dnf -y install dnf-plugins-core 2> /dev/null
 sudo dnf -y copr enable dperson/neovim 2> /dev/null
 sudo dnf -y copr enable heliocastro/hack-fonts 2> /dev/null
-sudo dnf -y install vim i3 firefox git htop python2-greenlet-devel xclip emacs xfce4-terminal mutt neovim hack-fonts 2> /dev/null
+sudo dnf -y install \
+    vim \
+    i3 \
+    firefox \
+    git \
+    htop \
+    python2-greenlet-devel \
+    xclip \
+    emacs \
+    xfce4-terminal \
+    mutt \
+    neovim \
+    hack-fonts 2> /dev/null
 
 #Linux Mint
 sudo apt-get update && sudo apt-get upgrade 2> /dev/null
-sudo apt-get install vim i3-wm i3status i3lock i3 suckless-tools network-manager htop git xclip emacs xfce4-terminal mutt scrot lsb-release 2> /dev/null
+sudo apt-get install \
+    vim \
+    i3-wm \
+    i3status \
+    i3lock \
+    i3 \
+    suckless-tools \
+    network-manager \
+    htop \
+    git \
+    xclip \
+    emacs \
+    xfce4-terminal \
+    mutt \
+    scrot \
+    lsb-release 2> /dev/null
 
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 chmod +x udf.sh
